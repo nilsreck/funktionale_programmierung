08db0fc1d8f8ca1de8a4f8afd88875497100ac7e
nreck1 <nils.reck@hhu.de>
2021-11-01 16:01:27 +0100

    blatt 1 bis count

--- src/praesenz01/core.clj
+++ src/praesenz01/core.clj
@@ -1,7 +1,28 @@
 (ns praesenz01.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn flip [x y z]
+  [z y x])
+
+(let [random (rand)
+      random2 (* 2 random)]
+  [random random2])
+
+(defn fizzbuzz [x]
+  (if (and (= 0 (mod x 5)) (= 0 (mod x 3)))
+    :fizzbuzz
+    (if (= 0 (mod x 5))
+      :buzz
+      (if (= 0 (mod x 3))
+        :fizz x)))
+  )
+
+(defn counter [x]
+  (if (empty? x)
+    0
+    (+ (counter (rest x)) 1)))
+
+(defn count-x [seq x]
+  )
+
+
